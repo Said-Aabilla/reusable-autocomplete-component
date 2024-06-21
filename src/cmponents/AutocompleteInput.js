52c05866d861534aa38e77f3613c8b522196d424
Aabilla Said <said160aabilla@gmail.com>
2024-06-21 14:12:17 +0100

    added remove selection button

--- src/cmponents/AutocompleteInput.js
+++ src/cmponents/AutocompleteInput.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
-const AutocompleteInput = ({ query, setQuery, onKeyDown ,onFocus}) => {
+const AutocompleteInput = ({ query, setQuery, onKeyDown, onFocus }) => {
   return (
-    <input
-      type="text"
-      value={query}
-      onChange={(e) => setQuery(e.target.value)}
-      onKeyDown={onKeyDown}
-      onFocus={onFocus}
-      className="w-full border border-gray-300 p-2"
-      placeholder="Search..."
-    />
+    <div className="relative">
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyDown={onKeyDown}
+        onFocus={onFocus}
+        className="w-full border border-gray-300 p-2 pr-10"
+        placeholder="Search..."
+      />
+      {query && (
+        <button
+          onClick={() => setQuery('')}
+          className="absolute right-2 top-1/2 transform -translate-y-1/2"
+        >
+          <FontAwesomeIcon icon={faTimes} className="text-gray-500" />
+        </button>
+      )}
+    </div>
   );
 };
 
